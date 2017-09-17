cfc9e46ae7cfa948417957616cde47ec74b63c90
conmart <connor.a.martinelli@gmail.com>
2017-09-17 16:56:41 -0700

    working show, create

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,7 +1,29 @@
 class CreaturesController < ApplicationController
 
   def index
+    @creatures = Creature.all
+  end
 
+  def new
+    @creature = Creature.new
+  end
+
+  def show
+    @creature = find_one
+  end
+
+  def create
+    Creature.create(creature_params)
+    redirect_to creatures_path
+  end
+
+  def find_one
+    creature = Creature.find(params[:id])
+  end
+
+  private
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get "/creatures" => "creatures#index", as: 'creatures'
+  get "/creatures/new" => "creatures#new", as: 'new_creature'
+  get "/creatures/:id" => "creatures#show", as: 'creature'
+  post "/creatures" => "creatures#create"
 
 end
